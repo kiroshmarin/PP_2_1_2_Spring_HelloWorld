6c047f03089b8af26ee567c196e3af58eae075fe
Kirill_Oshmarin <k.oshmarin@gmail.com>
2023-06-02 17:00:27 +0300

    PP_2_1_3 Done!

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,16 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat =
+                (Cat) applicationContext.getBean("cat");
+        Cat cat1 =
+                (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean == bean1);
+        System.out.println(cat == cat1);
+
         System.out.println(bean.getMessage());
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String cat;
+
+    public String getMessage() {
+        return cat;
+    }
+
+    public void setMessage(String cat) {
+        this.cat = cat;
+    }
+}
